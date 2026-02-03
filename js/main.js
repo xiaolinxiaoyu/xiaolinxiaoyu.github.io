d969b6ac707d068da0136867689fd91f78f1864d
Hengzhi Yu <hihengzhi@foxmail.com>
2026-02-03 18:33:28 +0800

    UPDATE

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ const startDate = new Date("2025-11-25");
 const today = new Date();
 const diffTime = today - startDate;
 const days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-document.getElementById("days").innerText = `已经在一起 ${days} 天啦 💕`;
+document.getElementById("days").innerText = `已经在一起 ${days+1} 天啦 💕`;
 
