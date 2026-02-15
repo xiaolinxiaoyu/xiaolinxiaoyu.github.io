dd9de1bcf7ba996b3b1d83228670420cc29b7f16
aiwenjian <hihengzhi@foxmail.com>
2026-02-16 04:14:29 +0800

    update

--- js/main.js
+++ js/main.js
@@ -24,8 +24,8 @@ function getTogetherDays(todayDate) {
 
 function formatDate(date) {
   const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 }
 
